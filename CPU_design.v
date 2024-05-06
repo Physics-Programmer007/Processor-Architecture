433cfc42d72e9b65510df5837517deac616fa1ea
Shrikanth <160249883+Physics-Programmer007@users.noreply.github.com>
2024-05-06 11:00:16 +0530

    Add files via upload

---
+++ CPU_design.v
@@ -0,0 +1,26 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 05/06/2024 09:11:26 AM
+// Design Name: 
+// Module Name: CPU_design
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module CPU_design(
+
+    );
+endmodule
